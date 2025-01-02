92054448bce34454610db416eeaf8121519a1e6d
JUANJOSEDH028 <juanjosalazar0228@gmail.com>
2025-01-02 11:06:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -56,7 +56,7 @@ class NutritionTracker:
                         st.session_state['token'] = flow.credentials.to_json()
                         st.session_state['is_authenticated'] = True
                         st.success("¡Autorización exitosa!")
-                        st.experimental_rerun()
+                        st.session_state['is_authenticated'] = True
                     except Exception as e:
                         st.error(f"Error al procesar el código de autorización: {str(e)}")
                 else:
